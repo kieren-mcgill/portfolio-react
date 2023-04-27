bf4fc00afb6ec09d2cbf89d124dbbfbc1e3919f8
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2023-04-27 17:37:07 +0100

    added React project cards

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-const App = () => {
-  return (
-    <h1>Hello from React</h1>
-  );
-};
-
-export default App;
---
+++ src/projects.js
@@ -0,0 +1,27 @@
+
+
+const projects = [
+    {
+        title: 'Project title',
+        blurb: 'Further information about the project',
+        image: 'portfolio-pic.png',
+        GitHubRef: '',
+        GitHubPagesRef: ''
+    },
+    {
+        title: 'Project title',
+        blurb: 'Further information about the project',
+        image: 'portfolio-pic.png',
+        GitHubRef: '',
+        GitHubPagesRef: ''
+    },
+    {
+        title: 'Project title',
+        blurb: 'Further information about the project',
+        image: 'portfolio-pic.png',
+        GitHubRef: '',
+        GitHubPagesRef: ''
+    },
+]
+
+export default projects
