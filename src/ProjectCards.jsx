2f8fbd0cbdcbfa8549cea4c4fd1a0171c3468adb
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2023-06-09 13:59:11 +0100

    changes to skillsand projects

--- src/ProjectCards.jsx
+++ src/ProjectCards.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 
-const ProjectsCards = ({projectInfo}) => {
-    return (
-        projectInfo.map((project) => {
-            return (
-                <>
-                    <div className="card border-dark mb-3 me-3">
-                        <img className="card-img-top height: " src={`images/${project.image}`}
-                             alt="A screenshot of a portfolio project"/>
-                        <div className="card-body text-dark">
-                            <h5 className="card-title text-center red-el">{project.title}</h5>
-                            <p className="card-text text-center">{project.blurb}</p>
-                        </div>
-                        <div className="card-footer d-flex justify-content-around">
-                            <a href={project.URL} target="_blank" rel="noopener noreferrer">
-                                <i className="fa-solid fa-window-maximize fa-xl m-1 blue-el"></i>
-                                <small className="text-muted">Project</small>
-                            </a>
-                            <a href={project.GitHubRef} target="_blank" rel="noopener noreferrer">
-                                <i className="fa-brands fa-github fa-xl m-1 blue-el"></i>
-                                <small className="text-muted">Code</small>
-                            </a>
-                        </div>
-                    </div>
-                </>
-            )
-        })
-    )
+const ProjectsCards = ({ projectInfo }) => {
+  return (
+    projectInfo.map((project) => {
+      return (
+        <>
+          <div className="card border-dark mb-3 me-3">
+            <img className="card-img-top height: " src={`images/projects/${project.image}`}
+                 alt="A screenshot of a portfolio project"/>
+            <div className="card-body text-dark">
+              <h5 className="card-title text-center red-el">{project.title}</h5>
+              <p className="card-text text-center">{project.blurb}</p>
+            </div>
+            <div className="card-footer d-flex justify-content-around">
+              <a href={project.Url} target="_blank" rel="noopener noreferrer">
+                <i className="fa-solid fa-window-maximize fa-xl m-1 blue-el"></i>
+                <small className="text-muted">Project</small>
+              </a>
+              {project.GitHubFront !== '' &&
+                <a href={project.GitHubFront} target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-github fa-xl m-1 blue-el"></i>
+                  <small className="text-muted">Frontend</small>
+                </a>
+              }
+              {project.GitHubBack !== '' &&
+                <a href={project.GitHubBack} target="_blank" rel="noopener noreferrer">
+                  <i className="fa-brands fa-github fa-xl m-1 blue-el"></i>
+                  <small className="text-muted">Backend</small>
+                </a>
+              }
+            </div>
+          </div>
+        </>
+      )
+    })
+  )
 }
 
 export default ProjectsCards
--- src/projects.js
+++ src/projects.js
@@ -5,29 +5,9 @@ const projects = [
         title: 'Portfolio Website',
         blurb: "You're already here! A portfolio website built using Bootstrap and a bit of React.",
         image: 'portfolio_project.png',
-        GitHubRef: '#',
-        URL: 'https://www.kierenmcgill.co.uk'
-    },
-    {
-        title: 'Coming Soon...',
-        blurb: 'Further information about the project',
-        image: 'portfolio_project.png',
-        GitHubRef: '#',
-        URL: '#'
-    },
-    {
-        title: 'Coming Soon...',
-        blurb: 'Further information about the project',
-        image: 'portfolio_project.png',
-        GitHubRef: '#',
-        URL: '#'
-    },
-    {
-        title: 'Coming Soon...',
-        blurb: 'Further information about the project',
-        image: 'portfolio_project.png',
-        GitHubRef: '#',
-        URL: '#'
+        GitHubFront: 'test',
+        GitHubBack: '',
+        Url: 'https://www.kierenmcgill.co.uk'
     }
 ]
 
