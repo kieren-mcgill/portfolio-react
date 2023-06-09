0133bf186ebcc1c4c573e8a7750fc2501c59bce8
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2023-06-09 15:02:24 +0100

    added products

--- src/projects.js
+++ src/projects.js
@@ -5,10 +5,50 @@ const projects = [
         title: 'Portfolio Website',
         blurb: "You're already here! A portfolio website built using Bootstrap and a bit of React.",
         image: 'portfolio_project.png',
-        GitHubFront: 'test',
+        GitHubFront: 'https://github.com/kieren-mcgill/portfolio-react',
         GitHubBack: '',
         Url: 'https://www.kierenmcgill.co.uk'
-    }
+    },
+    {
+        title: 'Treats for Pets',
+        blurb: "A website built Pro Bono for a local pet shop and animal charity in Walkley, Sheffield. Built using React, Bootstrap, Axios, and Strapi for the backend",
+        image: 't4p-project.png',
+        GitHubFront: 'https://github.com/kieren-mcgill/treats-for-pets',
+        GitHubBack: '',
+        Url: 'https://darling-torte-929168.netlify.app'
+    },
+    {
+        title: 'UK Carbon Dashboard',
+        blurb: "A simple mobile web app displaying the UK's current energy mix. Built using vanilla Javascript and the National Grid's API",
+        image: 'power-app-project.png',
+        GitHubFront: 'https://github.com/kieren-mcgill/power-app',
+        GitHubBack: '',
+        Url: 'https://sprightly-treacle-84dd73.netlify.app'
+    },
+  {
+    title: 'Travelog',
+    blurb: "A desktop app the allows user to add, edit, delete and view travel diary entries. Built using vanilla Javascript",
+    image: 'travelog.png',
+    GitHubFront: 'https://github.com/kieren-mcgill/travel-journal',
+    GitHubBack: '',
+    Url: 'https://adorable-toffee-5ee04d.netlify.app'
+  },
+  {
+    title: 'Deeper than you Think',
+    blurb: "A text-based adventure game written in Java during a pair project, and played in the command line",
+    image: 'text-adventure.png',
+    GitHubFront: 'https://github.com/kieren-mcgill/java-text-adventure',
+    GitHubBack: '',
+    Url: ''
+  },
+  // {
+  //   title: '',
+  //   blurb: "",
+  //   image: '',
+  //   GitHubFront: '',
+  //   GitHubBack: '',
+  //   Url: ''
+  // }
 ]
 
 export default projects
